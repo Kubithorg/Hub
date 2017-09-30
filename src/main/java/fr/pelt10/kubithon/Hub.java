1909df663cddaf7d91e9fff9aa20a3da5740fee1
Pelt10 <pelt10@live.fr>
2017-09-30 17:32:26 +0200

    set world time to 6000

--- src/main/java/fr/pelt10/kubithon/Hub.java
+++ src/main/java/fr/pelt10/kubithon/Hub.java
@@ -3,9 +3,13 @@ package fr.pelt10.kubithon;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.gamerule.DefaultGameRules;
 
-@Plugin(id="kubithonhub", name = "KubithonHub", version = "1.0-SNAPSHOT", authors = "Pelt10", description = "Plugin de Gestion du Hub pour le projet Kubithon", url = "https://kubithon.org/")
+@Plugin(id = "kubithonhub", name = "KubithonHub", version = "1.0-SNAPSHOT", authors = "Pelt10", description = "Plugin de Gestion du Hub pour le projet Kubithon", url = "https://kubithon.org/")
 public class Hub {
     private Game game;
     private Logger logger;
@@ -14,5 +18,15 @@ public class Hub {
     public Hub(Game game, Logger logger) {
         this.game = game;
         this.logger = logger;
+    }
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent e) {
+        //World setup
+        game.getServer().getWorlds().stream().map(World::getProperties).forEach(properties -> {
+            properties.setGameRule(DefaultGameRules.DO_DAYLIGHT_CYCLE, "false");
+            properties.setWorldTime(6000);
+        });
+
     }
 }
