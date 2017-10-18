10e5e85d4d49a95550dca6d24262eeb3dbde50a0
Pelt10 <pelt10@live.fr>
2017-10-18 18:42:34 +0200

    move all data management

--- src/main/java/fr/pelt10/kubithon/Hub.java
+++ src/main/java/fr/pelt10/kubithon/Hub.java
@@ -1,46 +1,31 @@
 package fr.pelt10.kubithon;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.inject.Inject;
-import ninja.leaping.configurate.ConfigurationNode;
-import ninja.leaping.configurate.ConfigurationOptions;
-import ninja.leaping.configurate.commented.CommentedConfigurationNode;
-import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
-import ninja.leaping.configurate.loader.ConfigurationLoader;
+import fr.pelt10.kubithon.dataregistry.DataManager;
+import fr.pelt10.kubithon.gui.GuiManager;
+import fr.pelt10.kubithon.listeners.PlayerJoin;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
-import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.EventManager;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.gamerule.DefaultGameRules;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.file.Path;
-import java.util.UUID;
 
 @Plugin(id = "kubithonhub", name = "KubithonHub", version = "1.0-SNAPSHOT", authors = "Pelt10", description = "Plugin de Gestion du Hub pour le projet Kubithon", url = "https://kubithon.org/")
 public class Hub {
+    @Getter
     private Game game;
+    @Getter
     private Logger logger;
-    private final String hubID = "HUB_" + UUID.randomUUID();
-    private JedisPool jedisPool;
 
-    @Inject
-    @DefaultConfig(sharedRoot = true)
-    private Path config;
-
-    private ConfigurationLoader<CommentedConfigurationNode> loader;
-    private ConfigurationNode rootNode;
+    @Getter
+    private DataManager dataManager;
 
     @Inject
     public Hub(Game game, Logger logger) {
@@ -49,38 +34,14 @@ public class Hub {
     }
 
     /**
-     * During this state, the plugin gets ready for initialization.
+     * During this state, the plugin gets setup for initialization.
      * Access to a default logger instance and access to information regarding preferred configuration file locations is available.
      *
      * @param event
      */
     @Listener
     public void onGamePreInitialization(GamePreInitializationEvent event) {
-        logger.info("Loading data");
-        logger.info("Hub ID : {}", hubID);
-
-        logger.info("Loading ConfigFile");
-        loader = HoconConfigurationLoader.builder().setPath(config).build();
-        try {
-            rootNode = loader.load(ConfigurationOptions.defaults());
-
-            logger.info("Starting RedisPool System");
-            ConfigurationNode redisNode = rootNode.getNode("redis");
-            jedisPool = new JedisPool(new JedisPoolConfig(),
-                    redisNode.getNode("ip").getString(), //IP Address
-                    redisNode.getNode("port").getInt(), //Port
-                    5000,  //Timeout
-                    redisNode.getNode("password").getString(), //Password
-                    0, // Database
-                    hubID);
-
-            try (Jedis jedis = jedisPool.getResource()) {
-                jedis.ping();
-            }
-        } catch (IOException e) {
-            logger.trace(e.getMessage(), e);
-            game.getServer().shutdown();
-        }
+        dataManager = new DataManager(this);
     }
 
     /**
@@ -97,19 +58,7 @@ public class Hub {
             properties.setWorldTime(6000);
         });
 
-        Task.builder().execute(task -> {
-            JsonParser parser = new JsonParser();
-            JsonObject serverInfo = new JsonObject();
-            InetSocketAddress ip = game.getServer().getBoundAddress().get();
-            serverInfo.add("name", parser.parse(hubID));
-            serverInfo.add("ip", parser.parse(ip.getAddress().getHostAddress()));
-            serverInfo.add("port", parser.parse(Integer.toString(ip.getPort())));
-            try(Jedis jedis = jedisPool.getResource()) {
-                jedis.select(1);
-                jedis.hset("HUB", hubID, serverInfo.toString());
-                jedis.publish("HUB_PUBSUB", "NEW " + serverInfo.toString());
-            }
-        }).submit(this);
+        dataManager.setup();
 
     }
 
@@ -121,11 +70,6 @@ public class Hub {
      */
     @Listener
     public void onGameStoppingServer(GameStoppedServerEvent event) {
-        try (Jedis jedis = jedisPool.getResource()) {
-            jedis.select(1);
-            jedis.hdel("HUB", hubID);
-            jedis.publish("HUB_PUBSUB", "DELETE " + hubID);
-        }
-        jedisPool.destroy();
+        dataManager.unregister();
     }
 }
---
+++ src/main/java/fr/pelt10/kubithon/dataregistry/DataManager.java
@@ -0,0 +1,94 @@
+package fr.pelt10.kubithon.dataregistry;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.inject.Inject;
+import fr.pelt10.kubithon.Hub;
+import lombok.Getter;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.ConfigurationOptions;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.slf4j.Logger;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.scheduler.Task;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.UUID;
+
+public class DataManager {
+    @Getter
+    private final String hubID = "HUB_" + UUID.randomUUID();
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private Path config;
+    private ConfigurationLoader<CommentedConfigurationNode> loader;
+    private ConfigurationNode rootNode;
+
+    private Logger logger;
+    private Hub hub;
+    private JedisPool jedisPool;
+
+    public DataManager(Hub hub) {
+        this.hub = hub;
+        logger = hub.getLogger();
+
+        logger.info("Loading data");
+        logger.info("Hub ID : {}", hubID);
+
+        logger.info("Loading ConfigFile");
+        loader = HoconConfigurationLoader.builder().setPath(config).build();
+        try {
+            rootNode = loader.load(ConfigurationOptions.defaults());
+
+            logger.info("Starting RedisPool System");
+            ConfigurationNode redisNode = rootNode.getNode("redis");
+            jedisPool = new JedisPool(new JedisPoolConfig(),
+                    redisNode.getNode("ip").getString(), //IP Address
+                    redisNode.getNode("port").getInt(), //Port
+                    5000,  //Timeout
+                    redisNode.getNode("password").getString(), //Password
+                    0, // Database
+                    hubID);
+
+            try (Jedis jedis = jedisPool.getResource()) {
+                jedis.ping();
+            }
+        } catch (IOException e) {
+            logger.trace(e.getMessage(), e);
+            hub.getGame().getServer().shutdown();
+        }
+    }
+
+    public void setup() {
+        Task.builder().execute(task -> {
+            JsonParser parser = new JsonParser();
+            JsonObject serverInfo = new JsonObject();
+            InetSocketAddress ip = hub.getGame().getServer().getBoundAddress().get();
+            serverInfo.add("name", parser.parse(hubID));
+            serverInfo.add("ip", parser.parse(ip.getAddress().getHostAddress()));
+            serverInfo.add("port", parser.parse(Integer.toString(ip.getPort())));
+            try(Jedis jedis = jedisPool.getResource()) {
+                jedis.select(RedisKeys.getHUB_DB_ID());
+                jedis.hset(RedisKeys.getHUB_KEY_NAME(), hubID, serverInfo.toString());
+                jedis.publish(RedisKeys.getHUB_PUBSUB_CHANNEL(), "NEW " + serverInfo.toString());
+            }
+        }).submit(this);
+    }
+
+    public void unregister() {
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.select(RedisKeys.getHUB_DB_ID());
+            jedis.hdel(RedisKeys.getHUB_KEY_NAME(), hubID);
+            jedis.publish(RedisKeys.getHUB_PUBSUB_CHANNEL(), "DELETE " + hubID);
+        }
+        jedisPool.destroy();
+    }
+}
