76473b654a50381cf2e4b179981bbd8250d2b9dc
Pelt10 <pelt10@live.fr>
2017-11-29 23:49:08 +0100

    Rename HubInstance to ServerInstance

--- src/main/java/fr/pelt10/kubithon/hub/utils/ServerInstance.java
+++ src/main/java/fr/pelt10/kubithon/hub/utils/ServerInstance.java
@@ -1,9 +1,9 @@
-package fr.pelt10.kubithon.hub.dataregistry;
+package fr.pelt10.kubithon.hub.utils;
 
 import com.google.gson.Gson;
 import lombok.Getter;
 
-public class HubInstance {
+public class ServerInstance {
     private static Gson gson = new Gson();
     @Getter
     private String HubID;
@@ -12,17 +12,17 @@ public class HubInstance {
     @Getter
     private int port;
 
-    public HubInstance(String HubID, String ip, int port) {
+    public ServerInstance(String HubID, String ip, int port) {
         this.HubID = HubID;
         this.ip = ip;
         this.port = port;
     }
 
-    public static String serialize(HubInstance hubInstance) {
+    public static String serialize(ServerInstance hubInstance) {
         return gson.toJson(hubInstance);
     }
 
-    public static HubInstance deserialize(String string) {
-        return gson.fromJson(string, HubInstance.class);
+    public static ServerInstance deserialize(String string) {
+        return gson.fromJson(string, ServerInstance.class);
     }
 }
