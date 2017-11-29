f07614f9b36d8c753e486ec818ebe13f25ab449a
Pelt10 <pelt10@live.fr>
2017-11-29 23:55:43 +0100

    Change PlayerTeleportMessage to use ServerInstance

--- src/main/java/fr/pelt10/kubithon/hub/com/messages/PlayerTeleportMessage.java
+++ src/main/java/fr/pelt10/kubithon/hub/com/messages/PlayerTeleportMessage.java
@@ -4,6 +4,7 @@ import fr.pelt10.kubithon.hub.com.CommunicationMessage;
 import fr.pelt10.kubithon.hub.dataregistry.HubPubSub;
 import fr.pelt10.kubithon.hub.dataregistry.JedisUtils;
 import fr.pelt10.kubithon.hub.dataregistry.RedisKeys;
+import fr.pelt10.kubithon.hub.utils.ServerInstance;
 
 import java.util.UUID;
 
@@ -16,8 +17,8 @@ public class PlayerTeleportMessage implements CommunicationMessage {
 
     @Override
     public void send(Object[] datas) {
-        if (datas.length != 2 && datas[0] instanceof UUID && datas[1] instanceof String) {
-            jedisUtils.execute(jedis -> jedis.publish(RedisKeys.COMM_PUBSUB_CHANNEL, RedisKeys.COMM_PLAYER_TP + " " + ((UUID)datas[0]).toString() + " " + datas[1]));
+        if (datas.length != 2 && datas[0] instanceof UUID && datas[1] instanceof ServerInstance) {
+            jedisUtils.execute(jedis -> jedis.publish(RedisKeys.COMM_PUBSUB_CHANNEL, RedisKeys.COMM_PLAYER_TP + " " + ((UUID)datas[0]).toString() + " " + ServerInstance.serialize((ServerInstance)datas[1])));
         }
     }
 }
