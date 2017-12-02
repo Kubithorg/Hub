96854f8c9aef34f337537ccafdea6dccdf494994
Pelt10 <pelt10@live.fr>
2017-12-02 17:05:12 +0100

    Update HubMenu

--- src/main/java/fr/pelt10/kubithon/hub/gui/template/HubMenu.java
+++ src/main/java/fr/pelt10/kubithon/hub/gui/template/HubMenu.java
@@ -1,46 +1,69 @@
 package fr.pelt10.kubithon.hub.gui.template;
 
+
 import fr.pelt10.kubithon.hub.Hub;
 import fr.pelt10.kubithon.hub.com.messages.PlayerTeleportMessage;
 import fr.pelt10.kubithon.hub.gui.InventoryGUI;
 import fr.pelt10.kubithon.hub.utils.ServerInstance;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.item.EnchantmentData;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
 import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.enchantment.Enchantment;
+import org.spongepowered.api.item.enchantment.EnchantmentTypes;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
 
 import java.util.Arrays;
 import java.util.UUID;
 
 public class HubMenu extends InventoryGUI {
-    private Hub hub;
+    private Hub hubPlugin;
 
     public HubMenu(Hub hub) {
         super(hub);
-        this.hub = hub;
+        this.hubPlugin = hub;
     }
 
     @Override
     public void onAction(ClickInventoryEvent event) {
         event.getTransactions().stream().filter(slotTransaction -> slotTransaction.getOriginal().getType().equals(ItemTypes.BEACON)).forEach(slotTransaction -> {
             UUID uuid = event.getCause().first(Player.class).get().getUniqueId();
-            ServerInstance server = hub.getDataManager().getHub(slotTransaction.getOriginal().get(Keys.DISPLAY_NAME).orElse(Text.EMPTY).toPlainSingle()).get();
-            Object[] data = {uuid, server};
-            hub.getCommunicationManager().sendMessage(PlayerTeleportMessage.class, data);
+            ServerInstance server = hubPlugin.getDataManager().getHub(slotTransaction.getOriginal().get(Keys.ITEM_LORE).get().get(0).toPlainSingle()).get();
+            if (!hubPlugin.getDataManager().getHubInstance().getHubID().equals(server.getHubID())) {
+
+                Object[] data = {uuid, server};
+                hubPlugin.getCommunicationManager().sendMessage(PlayerTeleportMessage.class, data);
+            } else {
+                event.setCancelled(true);
+            }
         });
     }
 
     @Override
     public Inventory getInventory() {
         Inventory inv = getDefaultInventory(9, 6);
-        hub.getDataManager().getHubList().forEach(hub -> {
+
+        int i = 1;
+        for (ServerInstance hub : hubPlugin.getDataManager().getHubList()) {
             ItemStack itemStack = ItemStack.builder().itemType(ItemTypes.BEACON).build();
-            itemStack.offer(Keys.DISPLAY_NAME, Text.of(hub.getHubID()));
+            itemStack.offer(Keys.ITEM_LORE, Arrays.asList(Text.builder(hub.getHubID()).color(TextColors.GRAY).style(TextStyles.ITALIC).build()));
+            itemStack.offer(Keys.DISPLAY_NAME, Text.builder("Hub " + i).style(TextStyles.NONE).build());
+
+            if (this.hubPlugin.getDataManager().getHubInstance().getHubID().equals(hub.getHubID())) {
+                EnchantmentData enchantmentData = itemStack.getOrCreate(EnchantmentData.class).get();
+                enchantmentData.set(enchantmentData.enchantments().add(Enchantment.builder().type(EnchantmentTypes.SHARPNESS).level(1).build()));
+                itemStack.offer(enchantmentData);
+                itemStack.offer(Keys.HIDE_ENCHANTMENTS, true);
+            }
+
             inv.offer(itemStack);
-        });
+            i++;
+        }
 
         return inv;
     }
