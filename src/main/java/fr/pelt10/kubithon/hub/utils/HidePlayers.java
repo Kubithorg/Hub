8cec1ba0a21a09d0b5588c265a8d11763fd799c8
Pelt10 <pelt10@live.fr>
2017-12-06 14:15:41 +0100

    Add method to show player again

--- src/main/java/fr/pelt10/kubithon/hub/utils/HidePlayers.java
+++ src/main/java/fr/pelt10/kubithon/hub/utils/HidePlayers.java
@@ -1,8 +1,11 @@
 package fr.pelt10.kubithon.hub.utils;
 
 import fr.pelt10.kubithon.hub.Hub;
+import lombok.Getter;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.SPacketDestroyEntities;
+import net.minecraft.network.play.server.SPacketSpawnPlayer;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HidePlayers {
+    @Getter
     private List<Player> players = new ArrayList<>();
     private Hub hub;
     private final String PERMISSION = "kubithon.hub.displayplayer";
@@ -20,25 +24,36 @@ public class HidePlayers {
         hub.getGame().getEventManager().registerListeners(hub, this);
     }
 
+    @Listener
+    public void onConnect(ClientConnectionEvent.Join event) {
+        Player player = event.getTargetEntity();
+        if (!player.hasPermission(PERMISSION)) {
+            players.stream().map(p -> (EntityPlayerMP) p).forEach(p -> hidePlayer(p, player));
+        }
+    }
+
     public void hidePlayerFor(Player player) {
         players.add(player);
 
-        EntityPlayerMP thePlayer = (EntityPlayerMP)player;
+        EntityPlayerMP thePlayer = (EntityPlayerMP) player;
 
         hub.getGame().getServer().getOnlinePlayers().stream().filter(p -> !p.hasPermission(PERMISSION)).forEach(p -> {
             hidePlayer(thePlayer, p);
         });
     }
 
-    @Listener
-    public void onConnect(ClientConnectionEvent.Join event) {
-        Player player = event.getTargetEntity();
-        if(!player.hasPermission(PERMISSION)) {
-            players.stream().map(p -> (EntityPlayerMP)p).forEach(p -> hidePlayer(p, player));
+    private void hidePlayer(EntityPlayerMP player, Player hide) {
+        player.connection.sendPacket(new SPacketDestroyEntities(((EntityPlayerMP) hide).getEntityId()));
+    }
+
+    public void showPlayerFor(Player player) {
+        if (players.remove(player)) {
+            EntityPlayerMP thePlayer = (EntityPlayerMP) player;
+            hub.getGame().getServer().getOnlinePlayers().stream().filter(p -> !p.equals(player)).forEach(p -> showPlayer(thePlayer, player));
         }
     }
 
-    private void hidePlayer(EntityPlayerMP player, Player hide) {
-        player.connection.sendPacket(new SPacketDestroyEntities(((EntityPlayerMP)hide).getEntityId()));
+    private void showPlayer(EntityPlayerMP player, Player hide) {
+        player.connection.sendPacket(new SPacketSpawnPlayer((EntityPlayer) hide));
     }
 }
